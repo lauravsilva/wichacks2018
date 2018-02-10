a1fec085019b2c86c2412f6290ef0ed989b81eee
Laura Silva <lsilva@constantcontact.com>
2018-02-10 14:12:45 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 3000;
+var app = express();
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(port, function(err) {
+if (err) {
+    console.log(err);
+}
+});
